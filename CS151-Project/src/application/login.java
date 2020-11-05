ac16db26ebbdeb443702e7cb5fe0fac822a4e154
nikkomdoza <54650478+nikkomdoza@users.noreply.github.com>
2020-11-04 21:01:07 -0800

    Add login page

---
+++ CS151-Project/src/application/login.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class login extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("login.FXML"));
+			Scene scene = new Scene(root, 700, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
